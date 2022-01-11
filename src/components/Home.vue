276e9e41863732150556c7e09aaf00fae37ac2c3
Wilhan Azevedo <wilhan.azevedo@topsapp.com.br>
2022-01-11 00:33:30 -0400

    feat:add progress send mensage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,23 +4,24 @@
       :src="require('../assets/logo.png')"
       fluid
       alt="Responsive image"
-      style="margin-top:60px;"
+      style="margin-top: 60px"
     ></b-img>
     <b-card class="card" bg-variant="dark" text-variant="white">
       <b-card-body>
-        <div style="display: flex; width:100%;">
-        <b-form class="sel">
-          <label for="bairros">Bairro</label>
-          <div class="sel">
-            <b-form-select
-            class="select"
-              id="bairros"
-              label-field="bairros"
-              :options="options"
-            ></b-form-select>
-          </div>
-        </b-form>
-        <b-form class="sel">
+        <div style="display: flex; width: 50%">
+          <b-form class="sel">
+            <label for="bairros">Bairro</label>
+            <div class="sel">
+              <b-form-select
+                class="select"
+                id="bairros"
+                label-field="bairros"
+                :options="options"
+                @change="onChange($event)"
+              ></b-form-select>
+            </div>
+          </b-form>
+          <!-- <b-form class="sel">
           <label for="telefones">Numero</label>
           <div class="sel">
             <b-form-select
@@ -30,7 +31,7 @@
               :options="options"
             ></b-form-select>
           </div>
-        </b-form>
+        </b-form> -->
         </div>
         <b-form class="form">
           <b-form-group
@@ -42,11 +43,35 @@
             <b-form-textarea
               id="textarea"
               placeholder="Digite sua mensagem..."
+              v-model="mensagem"
+              rows="10"
             ></b-form-textarea>
           </b-form-group>
         </b-form>
+        <!-- <div class="input-group mb-1">
+          <input type="file" class="form-control" id="inputGroupFile02" accept=".jpg,.gif,.png" @change="changeFile()"/>
+        </div> -->
         <div class="button-group">
-          <b-button variant="primary" class="button">Enviar</b-button>
+          <p class="total">Total de contatos neste bairro: {{ total }}</p>
+          <b-button
+            variant="primary"
+            class="button"
+            style="margin-left: auto"
+            @click="enviar()"
+            >Enviar</b-button
+          >
+        </div>
+        <div>
+          <progress
+            id="bar"
+            class="progre"
+            value="0"
+            :max="total"
+            style="margin-top: 20px; width: 100%"
+          >
+            <span></span>
+          </progress>
+          <p class="totalenv">Numero de mensagens enviadas</p>
         </div>
       </b-card-body>
     </b-card>
@@ -54,16 +79,132 @@
 </template>
 
 <script>
+import axios from "axios";
+import Swal from "sweetalert2";
+
 export default {
+  mounted() {
+    axios({
+      method: "get",
+      url: "http://10.19.92.40:8000/api/list/todos",
+    }).then((response) => {
+      this.options = response.data;
+    });
+    axios({
+      method: "get",
+      url: "http://10.19.92.40:8000/api/lista",
+    }).then((response) => {
+      this.totais = response.data;
+    });
+  },
   data() {
     return {
-      options: ["Centro", "Imperial", "Paraíso", "Maringá"],
+      options: [],
+      totais: {},
+      total: 0,
+      bairro: "",
+      mensagem: "",
+      enviadas: 0,
+      file1: null,
     };
+  },
+  methods: {
+    onChange(e) {
+      this.total = this.totais[e];
+      this.bairro = e;
+    },
+    enviar() {
+      let data = {
+        bairro: this.bairro,
+        mensagem: this.mensagem,
+      };
+      let request = new XMLHttpRequest();
+      request.open("POST", "http://10.19.92.40:8000/api/enviar", true);
+      request.send(JSON.stringify(data));
+
+      Swal.fire({
+        title: "Enviando mensagem!",
+        text: "",
+        icon: "success",
+        confirmButtonText: "Ok",
+      });
+
+      console.log(this.file1);
+
+      setTimeout(() => {
+        this.getEnviadas();
+      }, 50000);
+    },
+    getEnviadas() {
+      // const keepAlive = (idInterval) => {
+      //  //this.enviadas++
+      //  axios({
+      //     method: "get",
+      //     url: "http://10.19.92.40:8000/api/status",
+      //   }).then((response) => {
+      //     this.enviadas = response.data[0].enviadas;
+
+      //     document.getElementById("bar").value = this.enviadas;
+      //     console.log(this.enviadas);
+
+      //     if (this.enviadas == 0) {
+      //       clearInterval(idInterval);
+      //       Swal.fire({
+      //         title: "Mensagens enviadas!",
+      //         text: "",
+      //         icon: "success",
+      //         confirmButtonText: "Ok",
+      //       });
+      //     }
+      //   });
+      // }
+      
+      // // aqui coloca uma variavel do data = intervalMSG
+      // // viu ai , passei a variavel 
+      // // lkkkk nome diferente satanas kkk  ia  perguntar aonde tava kkk, mas blz
+      // // tem q funfa
+      // let idint = setInterval(()=>{
+      //   keepAlive(idint)
+      // }, 5000)
+
+      let idInterval = setInterval(() => { 
+        axios({
+          method: "get",
+          url: "http://10.19.92.40:8000/api/status",
+        }).then((response) => {
+          this.enviadas = response.data[0].enviadas;
+
+          document.getElementById("bar").value = this.enviadas;
+          console.log(this.enviadas);
+
+          if (this.enviadas == 0) {
+            clearInterval(idInterval);
+            Swal.fire({
+              title: "Mensagens enviadas!",
+              text: "",
+              icon: "success",
+              confirmButtonText: "Ok",
+            });
+          }
+        });
+      }, 5000);
+    },
+    changeFile() {
+      this.file1 = document.getElementById("inputGroupFile02").files[0];
+    },
   },
 };
 </script>
 
 <style>
+.total {
+  font-size: 1.5rem;
+}
+
+.totalenv {
+  font-size: 0.8rem;
+}
+
 .card {
   margin-top: 20px;
 }
@@ -73,20 +214,34 @@ export default {
 }
 
 .button-group {
+  display: flex;
   text-align: right;
 }
 
-.select{
-    width: 100%;
-    height: 30px;
-    border-radius: 5px;
-    margin: 0 auto;
+.select {
+  width: 100%;
+  height: 30px;
+  border-radius: 5px;
+  margin: 0 auto;
 }
 
-.sel{
-    width: 100%;
-    margin: 0 auto;
-    margin-right: 20px;
+.sel {
+  width: 100%;
+  margin: 0 auto;
+  margin-right: 20px;
+  margin-bottom: 10px;
 }
 
+progress {
+  text-align: center;
+  height: 40px;
+  padding: 10px;
+}
+progress:after {
+  color: black;
+  font-size: 0.8rem;
+  font-weight: 600;
+  content: attr(value) "/" attr(max);
+  position: absolute;
+}
 </style>
